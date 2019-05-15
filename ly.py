ac1e67fae3d1b912031cd48201187ef1cc22a0fb
liuyang <ly90414@gmail.com>
2019-05-15 16:13:34 +0800

    '测试'

---
+++ ly.py
@@ -0,0 +1,15 @@
+#coding: utf-8
+import os
+import sys
+import json
+import numpy as np
+import datetime
+
+def test(value):
+	"""
+	"""
+	rst = 1 if value >0  else 0
+	return rst
+
+if __name__ == "__main__":
+	test(100)
